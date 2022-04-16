ce2deaa482c22db58f71efd33c6b6bc6c418cb57
Parker Weisbrich <102187881+Parker-Weisbrich@users.noreply.github.com>
2022-04-16 11:40:45 -0500

    First Commit ;

---
+++ app.js
@@ -0,0 +1,38 @@
+/**
+ * app.js
+ * Entry point file for scc-asgn-api application
+ * Project 3
+ * Name: Parker Weisbrich
+ * COMP2150 Web Services
+ */
+
+ let express = require('express')
+ let app = express();
+ const apiRouter = require("./api-router.js");
+ let mongoose = require("mongoose");
+ let bodyParser = require("body-parser");
+ 
+ // set up mongoose and body-parser
+ app.use(bodyParser.urlencoded({extended: true}));
+ 
+ app.use(bodyParser.json());
+ 
+ // connect to mongoose
+ mongoose.connect("mongodb://localhost/contacts", { useNewUrlParser: true});
+ var db = mongoose.connection;
+ 
+ if(!db) {
+     console.log("Error connecting to the DB.");
+ } else {
+     console.log("DB connected successfully");
+ }
+ 
+ var port = 8080;
+ 
+ app.use("/api", apiRouter);
+ 
+ app.get('/', (req, res) => res.send('Hello World with Express'));
+ 
+ app.listen(port, function () {
+     console.log("Running contacts-api on port " + port);
+ });
